cb0707022b82cb5c6c5a2b8150bf3dac509c9fe0
Dmitry <dima.salov.55@gmail.com>
2023-09-12 22:04:48 +0300

    sort Technologies and Headers

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <a href="#">Home</a>
+      <p />
+      <a href="#">News Feed</a>
+      <p />
+      <a href="#">Messages</a>
+      <p />
+    </div>
+  );
+}
+
+export default Header;
