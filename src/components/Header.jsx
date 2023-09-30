41cf092d054a7b5d6a4ef24ac0f5dcf4bd4a69a5
Dmitry <dima.salov.55@gmail.com>
2023-09-30 21:43:09 +0300

    changed the css files in the modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import s from "./Header.module.css"
+
 const Header = () => {
   return (
-    <div className="header">
+    <div className= {s.header}>
       <img src="https://www.adobe.com/content/dam/cc/us/en/creativecloud/design/discover/mascot-logo-design/mascot-logo-design_fb-img_1200x800.jpg"></img>
     </div>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,19 +1,20 @@
+import s from "./Profile.module.css"
+
 const Profile = () => {
    return (
-      <div className="content">
+      <div className={s.content}>
         <div>
           <img src="https://img.freepik.com/premium-photo/sunny-beach-in-paradise-with-white-sand-coconut-palms-and-a-blue-water-concept-of-summertime-fun-and-relaxation-on-a-tropical-beach_410516-19004.jpg"></img>
         </div>
-        <div>
-          {/* <img src="https://secretmag.ru/thumb/1200x0/filters:quality(75):no_upscale()/imgs/2023/02/09/16/5789107/06fae590f1d8cd395cd92a660ab510aa019e6399.jpg"></img> */}
+        <div className= {s.item}>
           ava + descriotion
         </div>
-        <div>
+        <div className= {`${s.item} ${s.active}`}>
           My posts
-          <div>New post</div>
-          <div>
-            <div>post 1</div>
-            <div>post 2</div>
+          <div className= {s.item}>New post</div>
+          <div className = {s.posts}>
+            <div className= {s.item}>post 1</div>
+            <div className= {s.item}>post 2</div>
           </div>
         </div>
       </div>
