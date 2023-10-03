c10aa222a844893e4c596d69cc112552251ec6bd
Dmitry <dima.salov.55@gmail.com>
2023-10-03 22:27:26 +0300

    added Route

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,12 +4,12 @@ import Post from "./Post/MyPost"
 const MyPosts = () => {
 
    return (
-        <div className= {s.item}>
+        <div className= { s.item }>
           My posts
-          <div className= {s.item}>New post</div>
-          <div className = {s.posts}>
-            <Post message='Hi, how are you?' count='15'/>
-            <Post message='Its my first post' count='20'/>
+          <div className= { s.item }>New post</div>
+          <div className = { s.posts }>
+            <Post message='Hi, how are you?' count='0'/>
+            <Post message='Its my first post' count='23'/>
           </div>
         </div>
    )
--- src/components/Profile/MyPosts/Post/MyPost.jsx
+++ src/components/Profile/MyPosts/Post/MyPost.jsx
@@ -3,11 +3,11 @@ import s from "./MyPost.module.css";
 const Post = (props) => {
 
   return (
-    <div className={s.item}>
+    <div className={ s.item }>
       <img src="https://png.pngtree.com/png-clipart/20211121/original/pngtree-funny-avatar-vector-icons-png-png-image_6948004.png"></img>
-      {props.message}
+      { props.message }
       <div>
-        <span>{props.count}</span>
+        <span>like { props.count }</span>
       </div>
     </div>
   );
