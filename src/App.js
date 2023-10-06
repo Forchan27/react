5671d5ae1b0a7b663b938854c148109ab6982128
Dmitry <dima.salov.55@gmail.com>
2023-10-06 22:31:34 +0300

    refactoring import

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import { BrowserRouter, Route } from "react-router-dom";
-import { Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
 
 const App = (props) => {
   return (
