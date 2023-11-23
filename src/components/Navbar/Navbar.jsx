db0d7ded16f186ac7a51e762eb8619007fefb341
Dmitry <dima.salov.55@gmail.com>
2023-11-23 23:23:43 +0300

    swap ahref on NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
 import s from "./Navbar.module.css"
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
    return (
       <nav className= {s.nav}>
         <div className = {s.item}>
-          <a href='/profile'>Profile</a>
-        </div>
-        <div className = {`${s.item} ${s.active}`}>
-          <a href='/dialogs'>Messages</a>
+          <NavLink to='/profile' className={navActive => navActive.isActive ? s.active : s.item}>Profile</NavLink>
         </div>
         <div className = {s.item}>
-          <a href='/news'>News</a>
+          <NavLink to='/dialogs' className={navActive => navActive.isActive ? s.active : s.item}>Messages</NavLink>
         </div>
         <div className = {s.item}>
-          <a href='/music'>Music</a>
+          <NavLink to='/news' className={navActive => navActive.isActive ? s.active : s.item}>News</NavLink>
         </div>
         <div className = {s.item}>
-          <a href='/settings'>Settings</a>
+          <NavLink to='/music' className={navActive => navActive.isActive ? s.active : s.item}>Music</NavLink>
+        </div>
+        <div className = {s.item}>
+          <NavLink to='/settings' className={navActive => navActive.isActive ? s.active : s.item}>Settings</NavLink>
         </div>
       </nav>
       )
